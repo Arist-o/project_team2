d8c0be14c011746e030463986323e89d8c8310f7
Aristo <xr564178@gmail.com>
2025-02-06 12:48:05 +0200

    with struct

--- project.cpp
+++ project.cpp
@@ -2,8 +2,19 @@
 #include <iostream>
 #include <Windows.h>
 using namespace std;
+struct sportsmen
+{
+    string prizv="";
+    string name = "";
+    double speed_run = 0;
+    double km3_run = 0;
+    int pres = 0;
+    double jump = 0;
+};
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    cout << R"(Виберіть що хочете зробити )" << endl;
+    
 }
